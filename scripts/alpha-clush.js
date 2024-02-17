7e78aae98461c14f44a5515292e1daeae1980b6b
mahmud2237 <smahmud2237@gmail.com>
2024-02-17 07:23:04 +0600

    Home screen hidden

---
+++ scripts/alpha-clush.js
@@ -0,0 +1,7 @@
+function play(){
+    // step-1: hide the home screen. to hide the screen add the class hidden to the home section.
+    const homeSection = document.getElementById('home-screen');
+    // console.log(homeSection.classList);
+    console.log(homeSection.classList.add('hidden'));
+    // show the playground
+}
