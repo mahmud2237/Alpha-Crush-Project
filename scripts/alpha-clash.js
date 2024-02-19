24599009300ce171581e430ad9f39fb70893dc05
mahmud2237 <smahmud2237@gmail.com>
2024-02-19 19:07:16 +0600

    playground and homescreen updated

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,102 @@
+// function play(){
+//     // step-1: hide the home screen. to hide the screen add the class hidden to the home section.
+//     const homeSection = document.getElementById('home-screen');
+//     // console.log(homeSection.classList);
+//     console.log(homeSection.classList.add('hidden'));
+//     // show the playground
+//     const playGroungSection = document.getElementById('play-ground');
+//     // console.log(playGroungSection.classList);
+//     // console.log(playGroungSection.classList.add('hidden'));
+//     // if want to remove any class
+//     console.log(playGroungSection.classList.remove('hidden'));
+// }
+
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    // get the key player is expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+    // console.log(currentAlphabetElement.innerText);
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you got a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        // console.log(currentScore);
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+        // update score:
+        // 1. get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+        
+        // // 2. increase the score by 1
+        // const newScore = currentScore + 1;
+        // // 3. show the updated score
+        // currentScoreElement.innerText = newScore;
+
+        // console.log('you have pressed correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed, so you lost a life');
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        if(updatedLife === 0){
+            gameOver();
+            console.log('game over');
+        }
+        // Lost Score:
+        // 1. get hte current score
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // // 2. decrease the score by 1
+        // const newLife = currentLife - 1;
+        // // 3. show the updated score
+        // currentLifeElement.innerText = newLife;
+        
+    }
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+// function handleKeyboardButtonPress(){
+//     console.log('button pressed');
+// }
+// // capture keyboard key press
+// document.addEventListener('keyup', handleKeyboardButtonPress);
+
+function continueGame(){
+    // step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    // console.log('Your random alphabet', alphabet);
+
+    //set randomly generated alphabet to the screen(show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // Set background color
+    setBackgroundColorById(alphabet);
+}
+
+
+function play(){
+    hideElementById('home-screen');
+    hideElementById('final-score');
+    showElementById('play-ground');
+    continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+}
